7fb76ee325e3b3406756398fff2c2b067f9fc165
suposk <jan.supolik@gmail.com>
2023-10-17 17:51:56 +0200

    intit single table

--- src/NetCoreBE.Api/Domain/Entities/Ticket.cs
+++ src/NetCoreBE.Api/Domain/Entities/Ticket.cs
@@ -3,8 +3,10 @@
 //public class Ticket : EntitySoftDeleteBase
 public class Ticket : EntityBase
 {
+    [MaxLength(1000)]
     public string? Description { get; set; }
 
+    [MaxLength(200)]
     public string? RequestedFor { get; set; }
 
     public bool IsOnBehalf { get; set; }
---
+++ src/NetCoreBE.Api/Infrastructure/Persistence/Migrations/SqlServerMigrations/20231017155047_Init.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetCoreBE.Api.Infrastructure.Persistence.Migrations.SqlServerMigrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(36)", maxLength: 36, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true),
+                    RequestedFor = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    IsOnBehalf = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_CreatedAt",
+                table: "Tickets",
+                column: "CreatedAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_CreatedBy",
+                table: "Tickets",
+                column: "CreatedBy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_Id",
+                table: "Tickets",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+        }
+    }
+}
