c0b41f2b16cb2ad9415a88877783f5fc98f818a8
suposk <jan.supolik@gmail.com>
2023-10-23 14:12:21 +0200

    IRepositoryCtx

--- src/NetCoreBE.Api/Infrastructure/Persistence/ApiBaseRepository.cs
+++ src/NetCoreBE.Api/Infrastructure/Persistence/ApiBaseRepository.cs
@@ -1,8 +1,17 @@
 ﻿namespace CSRO.Server.Services.Base;
 
-public class ApiBaseRepository<TModel> : Repository<TModel>, IRepository<TModel> where TModel : EntityBase
+public class ApiBaseRepository<TModel> : Repository<TModel> where TModel : EntityBase
 {
     public ApiBaseRepository(ApiDbContext context, IApiIdentity apiIdentity, IDateTimeService dateTimeService) : base(context, apiIdentity, dateTimeService)
+    {
+
+    }
+}
+
+
+public class ApiBaseRepositoryCtx<TModel, ApiDbContext> : RepositoryCtx<TModel, ApiDbContext> where TModel : EntityBase
+{
+    public ApiBaseRepositoryCtx(ApiDbContext context, IApiIdentity apiIdentity, IDateTimeService dateTimeService) : base(context, apiIdentity, dateTimeService)
     {
 
     }
