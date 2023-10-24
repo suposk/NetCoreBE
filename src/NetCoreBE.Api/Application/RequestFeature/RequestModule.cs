385596da4c64ee56fc071b98085eceb95e50c012
suposk <jan.supolik@gmail.com>
2023-10-24 16:37:20 +0200

    base route

--- src/NetCoreBE.Api/Application/RequestFeature/RequestModule.cs
+++ src/NetCoreBE.Api/Application/RequestFeature/RequestModule.cs
@@ -4,19 +4,19 @@ namespace NetCoreBE.Api.Application.RequestFeature;
 
 public class RequestModule : CarterModule
 {
-    //public RequestModule()
-    //    : base("api/request")
-    //{
-    //}
+    public RequestModule()
+        : base("api/request")
+    {
+    }
 
     public override void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("api/request", async (IRequestLogic _logic) =>
+        app.MapGet("/", async (IRequestLogic _logic) =>
         {
             return Results.Ok(await _logic.GetListLogic().ConfigureAwait(false));
         });
 
-        app.MapGet("api/request/{id}", async (string id, IRequestLogic _logic) =>
+        app.MapGet("/{id}", async (string id, IRequestLogic _logic) =>
         {
             return Results.Ok(await _logic.GetIdLogic(id).ConfigureAwait(false));
         });
