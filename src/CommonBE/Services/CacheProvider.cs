d39f35824a9554b56d6f4b8cd8c03eb0b03d7878
suposk <jan.supolik@gmail.com>
2023-10-25 19:22:58 +0200

    dict added

--- src/CommonBE/Services/CacheProvider.cs
+++ src/CommonBE/Services/CacheProvider.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
 using System.Runtime.CompilerServices;
+using System.Xml.Linq;
 
 namespace CommonBE.Services;
 
@@ -28,10 +30,47 @@ public interface ICacheProvider
     static string GetCacheKey(string userId, string className, [CallerMemberName] string caller = null) => $"{caller}||{userId}||{className}";
 }
 
+public class KeyIdPair
+{
+    public KeyIdPair(string key, string id)
+    {
+        Key = key;
+        Id = id;
+    }
+    public string? Key { get; set; }
+    public string? Id { get; set; }
+
+    #region GetHashCode and Equals
+
+    public override int GetHashCode()
+    {
+        return (Key != null && Id != null) ? Key.GetHashCode() ^ Id.GetHashCode() : Key.GetHashCode();
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null)
+            return false;
+        if (ReferenceEquals(obj, this))
+            return true;
+        if (obj.GetType() != GetType())
+            return false;
+
+        KeyIdPair rhs = obj as KeyIdPair;
+        return Key == rhs.Key && Id == rhs.Id;
+    }
+
+    public static bool operator ==(KeyIdPair lhs, KeyIdPair rhs) { return Equals(lhs, rhs); }
+    public static bool operator !=(KeyIdPair lhs, KeyIdPair rhs) { return !Equals(lhs, rhs); }
+
+    #endregion
+}
+
 public class CacheProvider : ICacheProvider
 {
     private readonly IMemoryCache _cache;
     private readonly ILogger<CacheProvider> _logger;
+    private readonly ConcurrentDictionary<KeyIdPair, object> _Dic = new();
 
     public CacheProvider(IMemoryCache cache, ILogger<CacheProvider> logger)
     {
@@ -58,37 +97,65 @@ public class CacheProvider : ICacheProvider
 
     public void SetCache<T>(string key, T value, int seconds = ICacheProvider.CacheSeconds) where T : class
     {
+        _Dic.AddOrUpdate(new KeyIdPair(key, value?.ToString()), value, (k, v) => value);
         _cache.Set(key, value, DateTimeOffset.Now.AddSeconds(seconds));
     }
 
     public void SetCache<T>(string key, string id, T value) where T : class
     {
-        if (string.IsNullOrWhiteSpace(id))
-            throw new ArgumentException($"'{nameof(id)}' cannot be null or whitespace.", nameof(id));
-
+        //if (string.IsNullOrWhiteSpace(id))
+        //    throw new ArgumentException($"'{nameof(id)}' cannot be null or whitespace.", nameof(id));
         SetCache($"{key}-{id}", value);
     }
 
     public void SetCache<T>(string key, string id, T value, int seconds = ICacheProvider.CacheSeconds) where T : class
     {
-        if (string.IsNullOrWhiteSpace(id))
-            throw new ArgumentException($"'{nameof(id)}' cannot be null or whitespace.", nameof(id));
-
+        //if (string.IsNullOrWhiteSpace(id))
+        //    throw new ArgumentException($"'{nameof(id)}' cannot be null or whitespace.", nameof(id));
         SetCache($"{key}-{id}", value, seconds);
     }
 
     public void ClearCache(string key)
     {
-        _cache.Remove(key);
+        
+        var allPairs = _Dic.Where(x => x.Key.Key == key).ToList();
+        if (allPairs.Count == 1 && allPairs.FirstOrDefault().Value == null)        
+            _cache.Remove(key);        
+        else
+        {
+            _cache.Remove(key);
+            foreach (var pair in allPairs)
+            {
+                //_Dic.TryRemove(pair.Key, out _);
+                _Dic.TryRemove(pair);
+                _cache.Remove($"{pair.Key}-{pair.Value}");
+            }
+        }
     }
 
     public void ClearCache(string key, string id)
     {
-        if (string.IsNullOrWhiteSpace(id))
-            throw new ArgumentException($"'{nameof(id)}' cannot be null or whitespace.", nameof(id));
+        //if (string.IsNullOrWhiteSpace(id))
+        //    throw new ArgumentException($"'{nameof(id)}' cannot be null or whitespace.", nameof(id));
+        //_cache.Remove($"{key}-{id}");
 
-        _cache.Remove($"{key}-{id}");
-
+        var allPairs = _Dic.Where(x => x.Key.Key == key).ToList();
+        if (allPairs.Count == 1 && allPairs.FirstOrDefault().Value == null)
+        {
+            _cache.Remove(key);
+            _cache.Remove($"{key}-{id}");
+        }
+        else
+        {
+            _cache.Remove(key);
+            _cache.Remove($"{key}-{id}");
+            foreach (var pair in allPairs)
+            {
+                //_Dic.TryRemove(pair.Key, out _);
+                _Dic.TryRemove(pair);
+                _cache.Remove($"{pair.Key}-{pair.Value}");
+            }
+        }
     }
 
     public async Task<T> GetOrAddAsync<T>(string key, int seconds, Func<Task<T>> taskFactory)
@@ -102,6 +169,7 @@ public class CacheProvider : ICacheProvider
 
         var res = await _cache.GetOrCreateAsync(key, async entry => await new AsyncLazy<T>(async () =>
         {
+            _Dic.AddOrUpdate(new KeyIdPair(key, string.Empty), string.Empty, (k, v) => string.Empty);
             entry.SlidingExpiration = TimeSpan.FromSeconds(seconds);
             return await taskFactory.Invoke();
         }).Value);
@@ -118,6 +186,7 @@ public class CacheProvider : ICacheProvider
         var comKey = $"{key}-{id}";
         var res = await _cache.GetOrCreateAsync(comKey, async entry => await new AsyncLazy<T>(async () =>
         {
+            _Dic.AddOrUpdate(new KeyIdPair(key, id?.ToString()), id, (k, v) => id);
             entry.SlidingExpiration = TimeSpan.FromSeconds(seconds);
             return await taskFactory.Invoke();
         }).Value);
