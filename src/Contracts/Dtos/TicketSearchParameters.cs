b20ec04f976ee77beb576d13b94e3965754d3ef3
suposk <jan.supolik@gmail.com>
2023-10-30 10:31:57 +0100

    TicketSearchParameters in contracts

---
+++ src/Contracts/Dtos/TicketSearchParameters.cs
@@ -0,0 +1,11 @@
+﻿namespace Contracts.Dtos
+{
+    /// <summary>
+    /// Inherts from SearchParameters
+    /// </summary>
+    public sealed class TicketSearchParameters : SearchParameters
+    {
+        public string? Description { get; set; }
+        //public override string OrderBy { get; set; } = nameof(TicketDto.Id);
+    }
+}
--- src/Contracts/GlobalUsings.cs
+++ src/Contracts/GlobalUsings.cs
@@ -1,5 +1,6 @@
 ﻿global using System.Text;
 global using System.Text.Json;
 global using Contracts.Dtos;
+global using Contracts.Common;
 
 global using SharedKernel.Base;
