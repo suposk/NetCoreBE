2c1a0999d8d678c01315295a64d346f14c360ccb
suposk <jan.supolik@gmail.com>
2023-11-01 09:40:19 +0100

    OutboxMessageDomaintEvent

---
+++ src/Server/NetCoreBE.Api/Domain/Entities/OutboxMessageDomaintEvent.cs
@@ -0,0 +1,19 @@
+﻿namespace NetCoreBE.Api.Domain.Entities;
+
+/// <summary>
+/// OutboxMessage pattern for some DomaintEvent
+/// </summary>
+public sealed class OutboxMessageDomaintEvent : EntityBase
+{
+    public const int MaxRetryCount = 5;
+
+    public bool? IsSuccess { get; set; }
+    public string? Error { get; set; }
+    public string Type { get; set; }
+    public string? TypeDetail { get; set; }
+    public string? Content { get; set; }
+    public DateTime OccuredUtc { get; set; }
+    public DateTime? ProcessedUtc { get; set; }
+    public int RetryCount { get; set; }
+    public DateTime? NextRetryUtc { get; set; }
+}
