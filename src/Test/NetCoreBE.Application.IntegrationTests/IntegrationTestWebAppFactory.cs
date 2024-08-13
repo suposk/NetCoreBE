5c9141ce061034ce22c4fa54391cee0ed172746f
Jan Supolik <jan.supolik@hotmail.com>
2024-08-13 17:51:21 +0200

    seed works

---
+++ src/Test/NetCoreBE.Application.IntegrationTests/IntegrationTestWebAppFactory.cs
@@ -0,0 +1,53 @@
+﻿using NetCoreBE.Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+//using Testcontainers.Keycloak;
+using Testcontainers.PostgreSql;
+using NetCoreBE.Api;
+using NetCoreBE.Infrastructure.Persistence;
+using NetCoreBE.Application.Interfaces;
+//using Testcontainers.Redis;
+
+namespace NetCoreBE.Application.IntegrationTests;
+
+public class IntegrationTestWebAppFactory : WebApplicationFactory<ProgramApi>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:latest")
+        .WithDatabase("netcorebe")
+        .WithUsername("postgres")
+        .WithPassword("postgres")
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.RemoveAll(typeof(DbContextOptions<ApiDbContext>));
+            services.RemoveAll(typeof(IApiDbContext));
+
+            string connectionString = $"{_dbContainer.GetConnectionString()};Pooling=False";
+
+            services.AddDbContext<ApiDbContext>(options =>
+                options
+                    .UseNpgsql(connectionString)
+                    .UseSnakeCaseNamingConvention());
+
+            services.AddScoped<IApiDbContext>(provider => provider.GetService<ApiDbContext>());
+        });
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _dbContainer.StopAsync();
+    }
+}
