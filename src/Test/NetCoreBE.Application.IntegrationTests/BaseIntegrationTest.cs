89a7655a5fef34aab2c7cf8b5a655e97488f14f7
Jan Supolik <jan.supolik@hotmail.com>
2024-08-20 15:51:57 +0200

    BaseIntegrationTest

--- src/Test/NetCoreBE.Application.IntegrationTests/BaseIntegrationTest.cs
+++ src/Test/NetCoreBE.Application.IntegrationTests/BaseIntegrationTest.cs
@@ -6,16 +6,17 @@ namespace NetCoreBE.Application.IntegrationTests;
 
 public abstract class BaseIntegrationTest : IClassFixture<IntegrationTestWebAppFactory>
 {
-    private readonly IServiceScope _scope;
+    protected readonly IServiceScope Scope;
     protected readonly ISender Sender;
     protected readonly ApiDbContext DbContext;
 
     protected BaseIntegrationTest(IntegrationTestWebAppFactory factory)
     {
-        _scope = factory.Services.CreateScope();
+        Scope = factory.Services.CreateScope();
 
-        Sender = _scope.ServiceProvider.GetRequiredService<ISender>();
-        DbContext = _scope.ServiceProvider.GetRequiredService<ApiDbContext>();
+        Sender = Scope.ServiceProvider.GetRequiredService<ISender>();
+        DbContext = Scope.ServiceProvider.GetRequiredService<ApiDbContext>();
+
         DbContext.Database.EnsureDeleted();
         DbContext.Database.EnsureCreated();
     }
--- src/Test/NetCoreBE.Application.IntegrationTests/Tickets/TicketIntegrationTest.cs
+++ src/Test/NetCoreBE.Application.IntegrationTests/Tickets/TicketIntegrationTest.cs
@@ -1,28 +1,15 @@
-﻿using MediatR;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using NetCoreBE.Application.Tickets;
-using NetCoreBE.Infrastructure.Persistence;
 
 namespace NetCoreBE.Application.IntegrationTests.Tickets;
 
-public abstract class TicketIntegrationTest : IClassFixture<IntegrationTestWebAppFactory>
+public abstract class TicketIntegrationTest : BaseIntegrationTest, IClassFixture<IntegrationTestWebAppFactory>
 {
-    private readonly IServiceScope _scope;
-    protected readonly ISender Sender;
-    protected readonly ApiDbContext DbContext;
     protected readonly ITicketRepository TicketRepository;
 
-    protected TicketIntegrationTest(IntegrationTestWebAppFactory factory)
+    protected TicketIntegrationTest(IntegrationTestWebAppFactory factory) : base(factory)
     {
-        _scope = factory.Services.CreateScope();
-
-        Sender = _scope.ServiceProvider.GetRequiredService<ISender>();
-        DbContext = _scope.ServiceProvider.GetRequiredService<ApiDbContext>();
-
-        DbContext.Database.EnsureDeleted();
-        DbContext.Database.EnsureCreated();
-        TicketRepository = _scope.ServiceProvider.GetRequiredService<ITicketRepository>();
+        TicketRepository = Scope.ServiceProvider.GetRequiredService<ITicketRepository>();
         var seed = TicketRepository.Seed(4, 4, "SEED Startup").Result;
     }
 }
