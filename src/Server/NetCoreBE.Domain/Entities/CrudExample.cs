e6bc93f77bc9a622875ef99d35ff8a09b769cd94
Jan Supolik <jan.supolik@hotmail.com>
2024-09-04 09:55:10 +0200

    Add CrudExample class with nullable properties
    Introduced a new class `CrudExample` in the `NetCoreBE.Domain.Entities` namespace. This class inherits from `EntityBase` and includes three nullable properties: `IsActive` (bool?), `Name` (string?), and `Description` (string?).

---
+++ src/Server/NetCoreBE.Domain/Entities/CrudExample.cs
@@ -0,0 +1,8 @@
+﻿namespace NetCoreBE.Domain.Entities;
+
+public class CrudExample : EntityBase
+{
+    public bool? IsActive { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+}
