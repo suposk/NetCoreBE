01be4f7811f4af303a759eb55e21d590047524db
Jan Supolik <jan.supolik@hotmail.com>
2025-03-20 16:18:02 +0100

    Refactor Ticket class and related enums
    - Added a new `Ticket` class with a constant for initial status and a method to create an empty ticket.
    - Updated `StatusEnum` property to return `null` for empty status.
    - Changed ticket creation to use object initializer and ensure non-null notes.
    - Modified `Init` method to use a constant for initial status and correctly add notes.
    - Improved note handling in `Update` method and updated history operations.
    - Updated `TicketData` to use `TicketTypeEnum.Access` and a new ticket ID.
    - Adjusted tests in `TicketTest` to reflect changes in status and history assertions.

--- src/Test/NetCoreBE.Domain.UnitTests/Tickets/TicketData.cs
+++ src/Test/NetCoreBE.Domain.UnitTests/Tickets/TicketData.cs
@@ -1,9 +1,12 @@
 ﻿namespace NetCoreBE.Domain.UnitTests.Tickets;
 
+/// <summary>
+/// Domian data for Ticket
+/// </summary>
 internal static class TicketData
 {
     public static readonly string TicketId = "Ticket-1";
     public static readonly string AddTicketId = "Ticket-01";
 
-    public static readonly Ticket Ticket = Ticket.Create(TicketId, "New Laptop", "Description Post", "test");
+    public static readonly Ticket Ticket = Ticket.Create(AddTicketId, TicketTypeEnum.Access.ToString(), "Description Post", "test");
 }
